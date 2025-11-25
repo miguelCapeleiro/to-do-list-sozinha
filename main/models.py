6d4d03d4e2170bbb42c0f9af9dd79ee5206ac045
miguel <miguelcapeleiro2025@gmail.com>
2025-11-25 10:55:56 -0300

    tabela
    tabela criada

--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        BAIXA = "B", "BAIXA"
+        MEDIA = "M", "MEDIA"
+        ALTA = "A", "ALTA"
+        
+    
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        null=True,
+        blank=True
+    )
+    
+    titulo = models.CharField("Título", max_length=100)
+    descricao = models.TextField("Descrição", null=True, blank=True)
+    concluida = models.BooleanField("Concluída", default=False)
+    prioridade = models.CharField(
+        "Prioridade",
+        max_length=1,
+        choices=Priority.choices,
+        default=Priority.MEDIA
+    )
+    data_limite = models.DateField("Data Limite", null=True, blank=True)
+    criado_em = models.DateTimeField("Criado Em", auto_now_add=True)
+    atualizado_em = models.DateTimeField("Atualizado Em", auto_now=True)
+    
+    class Meta:
+        ordering = ["concluida", "data_limite", "-prioridade", "-criado_em"]
+        
+        def __str__(self) -> str:
+            return self.titulo
