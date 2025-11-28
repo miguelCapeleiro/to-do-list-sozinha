de863748958396207579cae02d3e187afa7840d7
Miguel <miguelcapeleiro2025@gmail.com>
2025-11-27 23:11:07 -0300

    parte final da ativade

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('concluidas', views.tasks_ok, name= 'tasks_ok'),
     path('pendentes', views.tasks_nok, name = 'tasks_nok'),
     path('home', views.home, name = 'home'),
-    path('baseform', views.task_create, name='baseform')
+    path('task_create', views.task_create, name='task_form'),
+    path('update_task/<int:pk>', views.task_update, name='task_update'),
+    path('delete_task/<int:pk>', views.task_delete, name='task_delete')
 ]
 
     
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 # Create your views here.
 
@@ -44,9 +44,59 @@ def task_create(request):
         concluida = request.POST.get('concluida', '') =='on'
         data_limite = request.POST.get('data_limite', '').strip()
         prioridade = request.POST.get('prioridade', '').strip()
+        
+        Task.objects.create(
+            titulo=titulo,
+            descricao=descricao,
+            concluida=concluida,
+            prioridade=prioridade,
+            data_limite=data_limite
+        ) 
+        
+        return redirect ('task_list')
+    
     
     context = {
         'opcoes_prioridade': Task.Priority.choices,
     }
 
     return render(request, 'tasks/task_form.html', context)
+
+
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk = pk)
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo', '').strip()
+        descricao = request.POST.get('descricao', '').strip()
+        concluida = request.POST.get('concluida', '') =='on'
+        data_limite = request.POST.get('data_limite', '').strip()
+        prioridade = request.POST.get('prioridade', '').strip()
+        
+        
+        task.titulo=titulo
+        task.descricao=descricao
+        task.concluida=concluida
+        task.prioridade=prioridade
+        task.data_limite=data_limite
+        task.save()
+        
+        return redirect ('task_list')
+    
+    
+    context = {
+        'opcoes_prioridade': Task.Priority.choices,
+        'task': task,
+    }
+
+    return render(request, 'tasks/task_form.html', context)
+
+
+
+def task_delete (request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    
+    if request.method == 'POST':
+        task.delete()
+        return redirect(task_list)
+    return render(request, 'tasks/task_confirm_delete.html')
